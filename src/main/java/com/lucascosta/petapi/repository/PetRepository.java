8d894e72dba1dc7c8a8dccebee0145602d8f053b
Lucas Costa <lg1352439@gmail.com>
2026-04-22 16:15:22 -0300

    feat: adiciona camada de repository

---
+++ src/main/java/com/lucascosta/petapi/repository/PetRepository.java
@@ -0,0 +1,11 @@
+package com.lucascosta.petapi.repository;
+
+import com.lucascosta.petapi.domain.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, UUID> {
+}
